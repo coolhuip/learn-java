6c0f9150d958db1b533495083f2a9a2be3b9c271
Danny Han <cool.huip@gmail.com>
2023-04-19 03:31:41 -0400

    added arrayListMethods()

--- src/main/app/codge/basicjava/CommonMethods.java
+++ src/main/app/codge/basicjava/CommonMethods.java
@@ -4,6 +4,8 @@
 package app.codge.basicjava;
 
 
+import java.util.ArrayList;
+
 import static app.codge.utilities.Utils.print;
 import static app.codge.utilities.Utils.newline;
 
@@ -12,6 +14,9 @@ import static app.codge.utilities.Utils.newline;
  */
 public class CommonMethods {
 
+    /**
+     * Method containing common String methods.
+     */
     public void stringMethods() {
         String str = "   Hello, World! ";
 
@@ -33,6 +38,33 @@ public class CommonMethods {
         str.toUpperCase();   //--> "   HELLO, WORLD! "
         str.trim();          //--> "Hello, World!"
         str.replace('l', 'f');   //--> "   Heffo, Worfd! "
+
+    }
+
+    /**
+     * Method containing common ArrayList methods
+     */
+    public void arrayListMethods() {
+        String[] colors = {"green", "red", "orange"};
+        int arrayLen = colors.length;
+
+        ArrayList<String> foods = new ArrayList<>();
+        int arrayListLen = foods.size();
+
+        foods.add("kimchi");
+        foods.add("chow-mein");
+        foods.add("sushi");
+
+        // foods.set(3, "mint");   *** IndexOutOfBoundsException ***
+        foods.set(1, "kung pow chicken");
+
+        for (int i = 0; i < foods.size(); i++) {
+            print(foods.get(i));
+        } newline();
+
+        int index = 0;
+        foods.remove(index);
+        foods.clear();
 
     }
 
